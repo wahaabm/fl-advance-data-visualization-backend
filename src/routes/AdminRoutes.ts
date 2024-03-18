6c71271f6d2d567735828fcdd823b2e96ec9eef2
Malik Abdul Wahab <malikwahab34@gmail.com>
2024-03-18 20:22:53 +0500

    fixed a small bug in router

--- src/routes/AdminRoutes.ts
+++ src/routes/AdminRoutes.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import { login, register } from "../controllers/AuthController";
 import checkToken from "../middlewares/checkToken";
-import { allowUser, app, showUsers } from "../controllers/AdminController";
+import {
+  allowUser,
+  app,
+  makeEditor,
+  removeEditor,
+  revokeUser,
+  showUsers,
+} from "../controllers/AdminController";
 
 const adminRouter = express.Router();
 
 adminRouter.get("/check", checkToken, app);
 adminRouter.get("/showUsers", checkToken, showUsers);
 adminRouter.get("/allowUser/:userId", checkToken, allowUser);
-adminRouter.get("/revokeAccess", checkToken, allowUser);
+adminRouter.get("/revokeUser/:userId", checkToken, revokeUser);
+adminRouter.get("/removeEditor/:userId", checkToken, removeEditor);
+adminRouter.get("/makeEditor/:userId", checkToken, makeEditor);
 
 export default adminRouter;
