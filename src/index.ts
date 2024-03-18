0b772243efbdab992bdbcbeaac35f56dba50fb4b
Malik Abdul Wahab <malikwahab34@gmail.com>
2024-03-18 05:38:42 +0500

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello worlds");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
