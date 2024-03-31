2abb9e612b2d3c62568f83b031b15a32c1665246
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-03-31 17:15:57 +0500

    update cors hosts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,29 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
-import bcrypt from "bcrypt";
-import { login } from "./controllers/AuthController";
-import authRouter from "./routes/authRoutes";
-import adminRouter from "./routes/ProtectedRoutes";
-import userRouter from "./routes/UserRoutes";
-dotenv.config();
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import authRouter from './routes/authRoutes'
+import adminRouter from './routes/ProtectedRoutes'
+import userRouter from './routes/UserRoutes'
+dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(
   cors({
-    origin: ['http://localhost:5173', '*'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: ['localhost', '64.23.225.23'],
+    // methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
+    optionsSuccessStatus: 200,
   })
 )
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use("/auth", authRouter);
-app.use("/admin", adminRouter);
-app.use("/", userRouter);
+app.use('/auth', authRouter)
+app.use('/admin', adminRouter)
+app.use('/', userRouter)
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
 })
