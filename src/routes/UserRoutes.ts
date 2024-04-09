c115bc3e699b464e58210b472df29455d03e944e
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-04-10 02:00:08 +0500

    add missing endpoint

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -3,11 +3,13 @@ import {
   readArticle,
   showArticles,
   showCharts,
+  showPinnedArticles,
 } from '../controllers/UserController'
 import checkToken from '../middlewares/checkToken'
 
 const userRouter = express.Router()
 
+userRouter.get('/pinned-articles', checkToken, showPinnedArticles)
 userRouter.get('/articles', checkToken, showArticles)
 userRouter.get('/article/:articleId', checkToken, readArticle)
 
