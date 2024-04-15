aa879d1af84ebca295aacde080d46a72dee8fc6e
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-04-15 20:29:10 +0500

    Update CORS allowed origins in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
-import authRouter from './routes/authRoutes'
 import adminRouter from './routes/ProtectedRoutes'
 import userRouter from './routes/UserRoutes'
+import authRouter from './routes/authRoutes'
 dotenv.config()
 
 const app = express()
@@ -19,9 +19,18 @@ app.use(
       'http://64.23.225.23',
       'http://64.23.225.23:3000',
       'http://64.23.225.23:5173',
+      'http://macrobourse.com',
+      'http://macrobourse.com:3000',
+      'http://macrobourse.com:5173',
       'http://www.macrobourse.com',
       'http://www.macrobourse.com:3000',
       'http://www.macrobourse.com:5173',
+      'https://macrobourse.com',
+      'https://macrobourse.com:3000',
+      'https://macrobourse.com:5173',
+      'https://www.macrobourse.com',
+      'https://www.macrobourse.com:3000',
+      'https://www.macrobourse.com:5173',
     ],
     // methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
